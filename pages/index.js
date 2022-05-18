ba1dc48a42c4db4498f0cd3eea53462fba0a37a1
이름 <rlawhdfhr7@naver.com>
2022-05-18 14:35:11 +0900

    :

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <Container>
           <Navbar.Brand href="#home">홈</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home">스테픈</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
